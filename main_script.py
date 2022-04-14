6e55f29c6d895d1068b17db080576a2caf3fdd33
James Galvagna <jgalvagn@umd.edu>
2022-04-14 13:40:17 -0400

    created classes/functions with doc strings

---
+++ main_script.py
@@ -0,0 +1,26 @@
+'''Our app will tell user about the weather of a city specified by user
+'''
+
+class City:
+    '''Holds information about the city that the user enters
+    Attributes - City that user wants the weather information of
+    '''
+    pass
+
+def Trend():
+    '''function that analyzes the weather data of the city entered
+    Args:
+        City entered by user fron city class
+    Returns:
+        Returns weather from city
+    '''
+    pass
+
+def api_access():
+    '''works with the openweather api
+    Args:
+        pass
+    Side Effects:
+        gets weather data from open weather api
+    '''    
+    pass
