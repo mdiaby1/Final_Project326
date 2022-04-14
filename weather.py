2ce79a470b611acd25ecf9b2cc0d94207618d343
mdiaby1 <moryd@wireless-10-105-207-134.umd.edu>
2022-04-14 13:57:25 -0400

    morys changes

---
+++ weather.py
@@ -0,0 +1,32 @@
+"""
+Before we start coding we need to get access to an OpenWeather API key, once we get that key we will be able to access
+OpenWeathers API and utilize it for our data.
+ Here's the link:https://openweathermap.org/
+ I'm not sure if we ALL need it because the free version only has a limited amount of calls, I will make an account 
+ and go through the  steps neccessary to call the API
+
+ This is our API key : 0cc0e4d9235938991e58db115667109b
+ Login info for the APU website:
+ `  user:practicehw237@gmail.com
+    pass:terrapinstrong123
+
+"""
+import requests # this import helps us request the data from the url I provided
+import json # this helps us with the OPEN Weather API json FILES that have the name of every city
+
+# we will develop our code more next week, but for this week we need some sort of unit testing
+#API accessor goes here/below and b4 the classes:
+
+
+
+
+
+class City: "Used to hold information about the city the user asked for"
+
+
+
+
+
+
+class Trend: """Used to perform data analysis on weather data (i.e., differences in certain aspects of the weather between two cities, or over a certain period of time)"""
+
